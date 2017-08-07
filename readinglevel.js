1c068409e46cfa1a40bb1b0081f343d088f3aef8
sirrah23 <hseth93@gmail.com>
2017-08-06 22:58:41 -0400

    Implemented file input and calculations

--- readinglevel.js
+++ readinglevel.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
 const rita = require('rita');
+const minimist = require('minimist');
 
 function count_syllables(word){
     return rita.getSyllables(word).split('/').length;
@@ -17,17 +19,24 @@ function text_metadata(text){
             return num_syllables + count_syllables(word);
         }, 0);
     }, 0);
-    return {total_sentences, total_words, total_syllables}
+    return {total_sentences, total_words, total_syllables};
 }
 
-function reading_ease({total_sentences, total_words, total_syllables}){
+function compute_reading_ease({total_sentences, total_words, total_syllables}){
     return 206.835 - 1.015 * (total_words/total_sentences) - 84.6 * (total_syllables/total_words);
 }
 
-function grade_level({total_sentences, total_words, total_syllables}){
+function compute_grade_level({total_sentences, total_words, total_syllables}){
     return 0.39 * (total_words/total_sentences) + 11.8 * (total_syllables/total_words) - 15.59;
 }
 
-const brownfox_metadata = text_metadata("The quick brown fox jumped over the lazy dog.")
-console.log("Reading ease is " + reading_ease(brownfox_metadata));
-console.log("Grade level is " + grade_level(brownfox_metadata));
+function main(filename){
+    const filecontent = fs.readFileSync(filename).toString();
+    const filecontent_metadata = text_metadata(filecontent);
+    const reading_ease = compute_reading_ease(filecontent_metadata)
+    const grade_level = compute_grade_level(filecontent_metadata);
+    console.log({reading_ease, grade_level});
+}
+
+const argv = minimist(process.argv.slice(2));
+main(argv.filename);
